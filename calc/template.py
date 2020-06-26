aa3d593654733b7a34b4b6302bd2664f06c67e1c
kmu-solhee <solhee0912@kookmin.ac.kr>
2020-06-27 02:11:39 +0900

    WSGI assignment sum&mul

---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = """
+<html>
+    <body>
+        <form actione="">
+            a : <input type="number" name="a"> 
+            b : <input type="number" name="b">
+            <input type="submit">
+        </form>
+        <p>
+            sum: %(sum)s</br>
+            mul: %(mul)s</br>
+        <p/>
+    </body>
+</html>
+"""
