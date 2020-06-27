abc2d2a041240178c6b2bef44fac0263c8ff08ba
kmu-solhee <solhee0912@kookmin.ac.kr>
2020-06-27 23:46:50 +0900

    issue #5 add sum = a+b

---
+++ Calc/calc.py
@@ -0,0 +1,23 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]
+    sum = 1
+    mul = 1
+    if '' not in [a, b]:
+        a, b = int(a), int(b)
+        sum = a+b
+        mul = a*b
+    response_body = html % {
+        'sum' : sum,
+        'mul' : mul,
+    }
+    start_response('200 OK',[
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
+
